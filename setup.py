654d417831fc2318feeaf648148f1747d686fee4
HaohanWang <haohanw@andrew.cmu.edu>
2017-07-07 15:02:24 -0400

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(
+      name='precisionLasso',
+      version='0.99',
+      author = "Haohan Wang",
+      author_email='haohanw@andrew.cmu.edu',
+      url = "https://github.com/HaohanWang/thePrecisionLasso",
+      description = 'Precision Lasso, Accounting for Correlations and Linear Dependencies in High-Dimensional Genomic Data',
+      packages=['models', 'utility'],
+      scripts=['runPL.py'],
+    )
